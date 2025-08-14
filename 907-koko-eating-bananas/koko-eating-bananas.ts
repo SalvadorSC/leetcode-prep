94b4af03a90aa32dbce2df4c1f717e69b2a542c8
Salvador A. Sánchez Campos <58109205+SalvadorSC@users.noreply.github.com>
2025-08-14 09:32:56 +0200

    Time: 11 ms (47.00%) | Memory: 59 MB (78.23%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.ts
+++ 907-koko-eating-bananas/koko-eating-bananas.ts
@@ -1,16 +1,24 @@
 function findMinEatingSpeed(
     piles: number[], target: number, start: number, end: number
 ): number {
+    // If we've reached a point where we can't keep searching, as we've cut from both sides, 
+    // either by start getting bigger or end getting smaller, we return the start. 
     if (start > end) {
         return start;
     }
 
     const middle = Math.floor((start + end) / 2);
+
+    // Calculate total piles divided between current speed of eating (middle) and ceil it. 
     const hoursWithKSpeed = piles.reduce((sum, pile) => sum + Math.ceil(pile / middle), 0)
 
+    // Recursion
+    // Check if the hours taken to eat at k speed are bigger than our target hours. 
     if (hoursWithKSpeed > target) {
+        // If they are, we search the second half.
         return findMinEatingSpeed(piles, target, middle + 1, end);
     } else {
+        // Else, we search the first half.
         return findMinEatingSpeed(piles, target, start, middle - 1);
     }
 }
@@ -21,32 +29,3 @@ function minEatingSpeed(piles: number[], h: number): number {
 
     return findMinEatingSpeed(piles, h, 1, biggestPile);
 };
-// console.log(minEatingSpeed([3, 6, 7, 11], 8));
-// console.log(minEatingSpeed([312884470], 312884469));
-// console.log(minEatingSpeed([332484035,524908576,855865114,632922376,222257295,690155293,112677673,679580077,337406589,290818316,877337160,901728858,679284947,688210097,692137887,718203285,629455728,941802184], 823855818));
-
-// var minEatingSpeed = function(piles, h) {
-//     let start = 1;
-//     let end = Math.max(...piles);
-//     
-//     while (start < end) {
-//         const mid = left + Math.floor((end - left) / 2);
-//         
-//         if (canEatAll(piles, h, mid)) {
-//             end = mid;
-//         } else {
-//             start = mid + 1;
-//         }
-//     }
-//     
-//     return left;
-// };
-// 
-// function canEatAll(piles, h, k) {
-//     let hours = 0;
-//     for (const pile of piles) {
-//         hours += Math.ceil(pile / k);
-//         if (hours > h) return false;
-//     }
-//     return true;
-// }
