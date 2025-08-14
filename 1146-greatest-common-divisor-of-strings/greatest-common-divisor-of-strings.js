50848142aaed19bd30132f63c7db0cc0588e1833
Salvador A. Sánchez Campos <58109205+SalvadorSC@users.noreply.github.com>
2025-08-14 10:59:06 +0200

    Time: 0 ms (100.00%) | Memory: 52.4 MB (98.39%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+
+
+var gcdOfStrings = function (str1, str2) {
+    let string1 = str1;
+    let string2 = str2;
+    
+
+    while (string1.length !== string2.length) {
+        if (string1.length > string2.length) {
+            if (string1.includes(string2)) {
+                string1 = string1.replace(string2, " ").trim()
+            } else return ''
+        } else {
+            if (string2.includes(string1)) {
+                string2 = string2.replace(string1, " ").trim()
+            } else return ''
+        }
+    }
+    return string1 === string2 ? string1 : ''
+    
+
+};
