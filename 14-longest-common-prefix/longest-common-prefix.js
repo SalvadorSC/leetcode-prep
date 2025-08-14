c161e908a1781be34e7990482466868734574b08
Salvador A. Sánchez Campos <58109205+SalvadorSC@users.noreply.github.com>
2025-08-14 09:27:32 +0200

    Time: 5 ms (17.30%) | Memory: 55.4 MB (36.90%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -3,19 +3,14 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
-    let commonPrefix;
+    let commonPrefix = '';
     const minimumLetters = strs.map((str)=> str.length).sort()[0]
-    const arrayWithoutFirstStr = strs.slice(1)
+    const choppedArr = strs.slice(1)
     for (let nLetters = 1; nLetters <= minimumLetters; nLetters++) {
         const constPrefix = strs[0].slice(0, nLetters);
-        if (arrayWithoutFirstStr.filter((str) => str.slice(0, nLetters) === constPrefix).length === arrayWithoutFirstStr.length) {
+        if (choppedArr.filter((str) => str.slice(0, nLetters) === constPrefix).length === choppedArr.length) {
             commonPrefix = constPrefix
         }
-    }
-
-
-    if (!commonPrefix) {
-        return ''
     }
 
     return commonPrefix
