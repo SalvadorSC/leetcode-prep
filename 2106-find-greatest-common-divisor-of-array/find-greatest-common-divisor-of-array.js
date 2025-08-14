328d41153bf25a325872623ede5f274118c8dd76
Salvador A. Sánchez Campos <58109205+SalvadorSC@users.noreply.github.com>
2025-08-14 10:36:10 +0200

    Time: 1 ms (43.25%) | Memory: 53.7 MB (80.09%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.js
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.js
@@ -3,24 +3,6 @@
  * @return {number}
  */
 
-function binaryFind(start, end, minNumber, maxNumber) {
-    if (start < end) {
-        return start;
-    }
-
-    const middle = Math.floor((start + end) / 2);
-    const target = (minNumber % middle) - (maxNumber % middle);
-
-    if (minNumber % middle === 0 && maxNumber % middle === 0) { return middle }
-console.log(middle )
-    // recursion
-    if (middle > target) {
-        binaryFind(start, middle + 1, minNumber, maxNumber)
-    } else {
-        binaryFind(middle-1, end, minNumber, maxNumber)
-    }
-}
-
 var findGCD = function (nums) {
     let gcd = 0;
     const minNumber = Math.min(...nums);
