2df1372823ffd6ac4e661d3a4cac6d63191d6574
Salvador A. Sánchez Campos <58109205+SalvadorSC@users.noreply.github.com>
2025-08-14 10:28:10 +0200

    Time: 1 ms (43.25%) | Memory: 54 MB (52.17%) - LeetSync

---
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findGCD = function (nums) {
+    let gcd = 0;
+    const minNumber = Math.min(...nums);
+    const maxNumber = Math.max(...nums);
+    for (let i = 1; i <= maxNumber; i++) {
+        if (minNumber % i === 0 && maxNumber % i === 0) {
+            gcd = i
+        }
+    }
+    return gcd;
+};
